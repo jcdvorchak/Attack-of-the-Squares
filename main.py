e56d34534123090277d82a129ef6a210746ee68a
Joey Dvorchak <jcdvorchak@gmail.com>
2015-01-23 16:24:45 -0600

    Add some todos

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# TODO:
+#	- make circles buttons
+#		- when pressed moveBullet# = True
+#		- when released moveBullet# = False
+#	- repetative code = function
+#	- move objects to separate file, game in another
+
+
 import random
 from kivy.app import App
 from kivy.uix.widget import Widget
@@ -51,6 +59,7 @@ class AttackGame(Widget):
 	def update(self, dt): # update the game
 		global squareSpeedMultiplier
 
+
 		# update score and square speed
 		self.score+=1
 		if (self.score==1000):
@@ -93,32 +102,37 @@ class AttackGame(Widget):
 		if(self.circle1.collide_widget(self.square1)|self.circle2.collide_widget(self.square2)|self.circle3.collide_widget(self.square3)|self.circle4.collide_widget(self.square4)):
 			pass # change to end game screen
 
-	def on_touch_move(self, touch): # move bullet when circle is touched
-		if (self.circle1.isTouched(touch)==True): # move only bullet 1
-			self.bullet1.move()
-			self.bullet2.reset(self.circle1)
-			self.bullet3.reset(self.circle1)
-			self.bullet4.reset(self.circle1)
-		elif (self.circle2.isTouched(touch)==True): # move only bullet 2
-			self.bullet2.move()
-			self.bullet1.reset(self.circle1)
-			self.bullet3.reset(self.circle1)
-			self.bullet4.reset(self.circle1)
-		elif (self.circle3.isTouched(touch)==True): # move only bullet 3
-			self.bullet3.move()
-			self.bullet1.reset(self.circle1)
-			self.bullet2.reset(self.circle1)
-			self.bullet4.reset(self.circle1)
-		elif (self.circle4.isTouched(touch)==True): # move only bullet 4
-			self.bullet4.move()
-			self.bullet1.reset(self.circle1)
-			self.bullet2.reset(self.circle1)
-			self.bullet3.reset(self.circle1)
-		else: # reset all bullets
-			self.bullet1.reset(self.circle1)
-			self.bullet2.reset(self.circle1)
-			self.bullet3.reset(self.circle1)
-			self.bullet4.reset(self.circle1)
+	def on_touch_down(self):
+		touch_down = True;
+
+	#def on_touch_down(self, touch): # move bullet when circle is touched
+	def touched(self, touch, touch_down):
+		if (touch_down==True):
+			if (self.circle1.isTouched(touch)==True): # move only bullet 1
+				self.bullet1.move()
+				self.bullet2.reset(self.circle1)
+				self.bullet3.reset(self.circle1)
+				self.bullet4.reset(self.circle1)
+			elif (self.circle2.isTouched(touch)==True): # move only bullet 2
+				self.bullet2.move()
+				self.bullet1.reset(self.circle1)
+				self.bullet3.reset(self.circle1)
+				self.bullet4.reset(self.circle1) 
+			elif (self.circle3.isTouched(touch)==True): # move only bullet 3
+				self.bullet3.move()
+				self.bullet1.reset(self.circle1)
+				self.bullet2.reset(self.circle1)
+				self.bullet4.reset(self.circle1)
+			elif (self.circle4.isTouched(touch)==True): # move only bullet 4
+				self.bullet4.move()
+				self.bullet1.reset(self.circle1)
+				self.bullet2.reset(self.circle1)
+				self.bullet3.reset(self.circle1)
+			else: # reset all bullets
+				self.bullet1.reset(self.circle1)
+				self.bullet2.reset(self.circle1)
+				self.bullet3.reset(self.circle1)
+				self.bullet4.reset(self.circle1)
 
 	def on_touch_up(self, touch): # reset all bullets on touch release
 		self.bullet1.reset(self.circle1)
@@ -145,7 +159,7 @@ class AttackApp(App):
 		# sm.add_widget(GameScreen(name='game'))
 
 		game = AttackGame()
-		Clock.schedule_interval(game.update, 1.0 / 60.0) #update the game every 1/60 of a second
+		Clock.schedule_interval(game.update, 1.0 / 30.0) #update the game every 1/60 of a second
 		return game
 
 if __name__ == '__main__':
